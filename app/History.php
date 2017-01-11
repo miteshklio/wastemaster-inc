6b4d562dbaf04763c4a585906a832fa45e6de83c
Lonnie Ezell <lonnieje@gmail.com>
2017-01-11 12:57:39 -0600

    Switching

---
+++ app/History.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    protected $table = 'history';
+
+    public $fillable = [
+        'lead_id', 'hauler_id', 'type'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function lead()
+    {
+        return $this->hasOne('App\Lead');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function hauler()
+    {
+        return $this->hasOne('App\Hauler');
+    }
+}
