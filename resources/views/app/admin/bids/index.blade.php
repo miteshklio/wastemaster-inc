b4992a6a808cc29d7aae751d6dad5b0ed4795939
Lonnie Ezell <lonnieje@gmail.com>
2017-01-12 11:28:23 -0600

    Fixing star showing up. Damn Laravel relationships and PHP7.

--- resources/views/app/admin/bids/index.blade.php
+++ resources/views/app/admin/bids/index.blade.php
@@ -30,8 +30,8 @@
                     <td>{{ $row->describeStatus() }}</td>
                     <td>
                         {{ $row->hauler_name }}
-                        <?php $lead = $row->lead; ?>
-                        @if (isset($lead->hauler) && $lead->hauler->name == $row->hauler_name)
+                        <?php $lead = $row->lead; $hauler = $lead->hauler; ?>
+                        @if (isset($lead->hauler) && $hauler->name == $row->hauler_name)
                             <img src="/img/star.png" class="hauler_star" alt="Current Hauler">
                         @endif
                     </td>
