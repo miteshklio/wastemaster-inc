80c89988a108e65537efde8aa64553377f6c64a4
Lonnie Ezell <lonnieje@gmail.com>
2017-01-12 09:16:44 -0600

    Fix error block to actually grab the 'error' from session.

--- resources/views/templates/_error_block.blade.php
+++ resources/views/templates/_error_block.blade.php
@@ -5,6 +5,13 @@
     </div>
 @endif
 
+@if( Session::has('error') )
+    <div class="alert alert-danger container">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <p><?php echo Session::get('error'); ?></p>
+    </div>
+@endif
+
 @if (count($errors) > 0)
     <div class="alert alert-danger container">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
