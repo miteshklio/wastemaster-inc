06d6054c199fc16e32d3e8682c3bd67223db3438
Lonnie Ezell <lonnieje@gmail.com>
2018-02-26 13:10:46 -0600

    Bigger font size on client landing page. T2302

--- resources/views/app/bids/form.blade.php
+++ resources/views/app/bids/form.blade.php
@@ -13,7 +13,7 @@
 
                 <br><br>
 
-                <p class="text-center">Thanks for your interest in submitting a bid.</p>
+                <p class="text-center lead">Thanks for your interest in submitting a bid.</p>
 
                 <p class="text-center">A bid has already been accepted, however, so we are not
                     taking any more bids for this company.</p>
@@ -25,7 +25,7 @@
 
                 <h2 class="text-center">Submit a bid for {{ $lead->company }}</h2>
 
-                <p class="text-center">{{ $lead->address }}</p>
+                <p class="text-center lead">{{ $lead->address }}</p>
 
                 <p class="text-center">{{ $lead->notes }}</p>
 
